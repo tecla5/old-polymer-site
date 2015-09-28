b2ddf49b3248511dcd3d5b8967ca96696db2150d
Kristian Mandrup <kmandrup@gmail.com>
2015-09-28 14:33:31 +0200

    trying to port polymer-lessons portfolio elements

--- app/elements/t5-home/home.js
+++ app/elements/t5-home/home.js
@@ -1,7 +1,35 @@
 (function() {
   Polymer({
     is: 't5-home',
+    behaviors: [
+      Polymer.NeonAnimationRunnerBehavior,
+      Polymer.NeonSharedElementAnimatableBehavior
+    ],
     properties: {
+      // START ANIMATIONS
+      animationConfig: {
+        value: function(){
+          return {
+            'entry': [{
+              name: 'fade-in-animation',
+              node: this
+            },{
+              name: 'transform-animation',
+              transformFrom: 'translateX(-100vh)',
+              node: this
+            }],
+            'exit': [{
+              name: 'hero-animation',
+              id: 'hero',
+              fromPage: this
+            },{
+              name: 'fade-out-animation',
+              node: this
+            }]
+          };
+        }
+      },
+      // END ANIMATIONS
       item: {
         type: Object,
         // when `items` changes `computeItem` is called once
@@ -18,7 +46,25 @@
       }
     },
     computeItem: function(items) {
-      return items[0];
-    }
+      return items && items[0] || {};
+    },
+    _repeatRender: function(){
+      this.paperCard = Polymer.dom(this.root).querySelectorAll('paper-card');
+      this.animationConfig.entry.push({
+        name: 'cascaded-animation',
+        animation: 'transform-animation',
+        transformFrom: 'translateY(100vh)',
+        nodes: this.paperCard
+      });
+    },
+    _openCard: function(event){
+      if(event.target.classList.contains('paper-fab')) {
+        this.fire('updateShowcase',this.productsList[event.model.__data__['item.id']]);
+        this.sharedElements = {
+          'hero': event.target
+        };
+        this.fire('changePage','showcase');
+      }
+    }    
   });
 })();
