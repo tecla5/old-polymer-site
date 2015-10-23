a32a837cde3d14085f00da54960363b70fd7f340
jcc2303 <cabrera.javier@gmail.com>
2015-10-23 17:19:11 +0200

    showing a post from tecla5 blog

--- app/elements/t5-blog/script.js
+++ app/elements/t5-blog/script.js
@@ -1,5 +1,28 @@
 (function() {
   Polymer({
     is: 't5-blog',
+
+    handleResponse: function(e) {
+      console.log('handleResponse', e);
+      var article = e.detail.response.querySelector('.post');
+      console.log('handleResponse', article);
+
+      //article.querySelector('.byline').remove();
+
+      // Fix up image paths to not be local.
+      [].forEach.call(article.querySelectorAll('img'), function(img) {
+        img.setAttribute('src', img.src);
+      });
+
+      var html = article.innerHTML;
+
+      //cache[ajax.url] = html; // Primitive caching by URL.
+      //this.injectBoundHTML(html, this.$.blog);
+      
+      this.$.blog.innerHTML = html;
+
+    }
+
   });
+
 })();
